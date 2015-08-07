d7de2c25a6e378cb9bab08c745fc6109bec83b77
Stanislav Stipl <stanislav.stipl@gmail.com>
2015-08-07 23:42:36 +0200

    gpg

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian:jessie
 MAINTAINER Stanislav Stipl, stanislav.stipl@gmail.com
 
 RUN apt-get update
-RUN apt-get install -y mariadb-client stunnel rsync
+RUN apt-get install -y mariadb-client stunnel rsync pgp
 
 COPY run.sh /
 COPY default_stunnel /etc/default/stunnel4
--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ while true
 do
   service stunnel4 restart
   mysqldump -u$MYSQL_ENV_DB_USER -p$MYSQL_ENV_DB_PASS -h $MYSQL_PORT_3306_TCP_ADDR $MYSQL_ENV_DB_NAME | gzip > /backup.sql.gz
+  gpg --keyserver pgp.mit.edu --recv-key 892D3F52
+  gpg --encrypt --recipient stanislav.stipl@gmail.com --yes --always-trust backup.sql.gz
   rsync -a -R /backup.sql.gz rsync://$WEDOS_USER@localhost/$WEDOS_USER/$WEDOS_FILENAME-`date +%Y-%m-%d_%H:%M`.sql.gz
   sleep +$WEDOS_INTERVAL
 done
